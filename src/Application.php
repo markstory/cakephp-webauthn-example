5c739447b376ec37be5e12a78bcd96e71a16afe0
Mark Story <mark@mark-story.com>
2023-05-21 23:27:53 -0400

    Fix redirect url

--- src/Application.php
+++ src/Application.php
@@ -155,7 +155,7 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
                 'prefix' => false,
                 'plugin' => null,
                 'controller' => 'Users',
-                'action' => 'startLogin',
+                'action' => 'login',
             ]),
             'queryParam' => 'redirect',
         ]);
