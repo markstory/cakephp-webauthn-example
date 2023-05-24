92025b83570459c44238a81592176e126921f746
Mark Story <mark@mark-story.com>
2023-05-23 22:34:50 -0400

    Use authentication everywhere.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -44,10 +44,6 @@ class AppController extends Controller
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
 
-        /*
-         * Enable the following component for recommended CakePHP form protection settings.
-         * see https://book.cakephp.org/4/en/controllers/components/form-protection.html
-         */
-        //$this->loadComponent('FormProtection');
+        $this->loadComponent('Authentication.Authentication');
     }
 }
