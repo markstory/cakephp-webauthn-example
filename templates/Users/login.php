ea5127688e37bc844d015045b8cabbcddc03e3b7
Mark Story <mark@mark-story.com>
2023-05-23 22:42:37 -0400

    More cleanup and UX improvements.

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -9,16 +9,25 @@ declare(strict_types=1);
 </div>
 <h1>Login</h1>
 <?php if (isset($user)): ?>
-    <h2>Logged in!</h2>
+    <h2>You are Logged in!</h2>
     <?php debug($user) ?>
 <?php endif; ?>
+<?php if ($this->request->getQuery('redirect')): ?>
+    <div class="message">You need to login</div>
+<?php endif; ?>
+
 <?php
 echo $this->Form->create();
 echo $this->Form->control('username');
 echo $this->Form->submit('Login');
 echo $this->Form->end();
+?>
 
-if (isset($loginData)): ?>
+<p> Need an account? 
+<?= $this->Html->link('Register', ['action' => 'startRegister']); ?>
+</p>
+
+<?php if (isset($loginData)): ?>
 <?= $this->element('webauthn-utils'); ?>
 <script type="text/javascript">
 async function completeLogin(loginData, csrfToken) {
@@ -51,3 +60,5 @@ async function completeLogin(loginData, csrfToken) {
 completeLogin(<?= json_encode($loginData->loginData); ?>, '<?= $this->request->getAttribute('csrfToken') ?>');
 </script>
 <?php endif ?>
+
+
--- templates/Users/view.php
+++ templates/Users/view.php
@@ -8,31 +8,22 @@
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit User'), ['action' => 'edit', $user->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete User'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Edit User'), ['action' => 'edit'], ['class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Logout'), ['action' => 'logout'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="users view content">
-            <h3><?= h($user->uuid) ?></h3>
+            <h3><?= h($user->username) ?></h3>
             <table>
                 <tr>
-                    <th><?= __('Uuid') ?></th>
-                    <td><?= h($user->uuid) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Username') ?></th>
-                    <td><?= h($user->username) ?></td>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($user->id) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Display Name') ?></th>
                     <td><?= h($user->display_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($user->id) ?></td>
                 </tr>
             </table>
             <div class="related">
